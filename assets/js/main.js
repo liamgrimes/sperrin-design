b644db71bdf2dd39b0f6d4278565228bc16c146a
LiamGrimes39 <55996969+LiamGrimes39@users.noreply.github.com>
2026-07-29 19:37:01 +0100

    Nav bar and basic js added

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    fetch("assets/components/navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar").innerHTML = data;
+        })
+        .catch(error => {
+            console.error("Navbar failed to load:", error);
+        });
+
+});
