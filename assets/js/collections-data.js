a88fab6f4362c9a65967506293ec0079cca61273
LiamGrimes39 <55996969+LiamGrimes39@users.noreply.github.com>
2026-08-18 20:58:01 +0100

    populate Festival Szn collection data

--- assets/js/collections-data.js
+++ assets/js/collections-data.js
@@ -43,7 +43,7 @@ window.SPERRIN_COLLECTIONS.COLLECTIONS = [
                 ],
                 description: "Collection piece from FW25.",
                 photographer: "TBC",
-                model: "TBC",
+                model: "Model 1",
                 available: false,
                 storeLink: ""
             }
@@ -63,7 +63,105 @@ window.SPERRIN_COLLECTIONS.COLLECTIONS = [
         slug: "festival-szn",
         description: "Sperrin Design Festival Szn collection.",
         coverImage: "assets/images/collections/festival-szn/cover-photo.JPG",
-        pieces: [],
-        editorial: []
+
+        pieces: [
+            {
+                name: "Blouse & Whimsy Shorts",
+                images: [
+                    "assets/images/collections/festival-szn/blouse-whimsy-shorts.JPG",
+                    "assets/images/collections/festival-szn/blouse-whimsy-shorts-2.JPG"
+                ],
+                description: "Collection piece from Festival Szn.",
+                photographer: "TBC",
+                model: "TBC",
+                available: false,
+                storeLink: ""
+            },
+
+            {
+                name: "Whimsy Blouse & Shorts",
+                images: [
+                    "assets/images/collections/festival-szn/whimsy-blouse-shorts.JPG",
+                    "assets/images/collections/festival-szn/whimsy-blouse-shorts-2.JPG"
+                ],
+                description: "Collection piece from Festival Szn.",
+                photographer: "TBC",
+                model: "Kathryn Nelson",
+                available: false,
+                storeLink: ""
+            },
+
+            {
+                name: "Culottes",
+                images: [
+                    "assets/images/collections/festival-szn/culottes.JPEG"
+                ],
+                description: "Collection piece from Festival Szn.",
+                photographer: "Broinnínn Devlin",
+                model: "TBC",
+                available: false,
+                storeLink: ""
+            },
+
+            {
+                name: "Double Denim Zip-Up",
+                images: [
+                    "assets/images/collections/festival-szn/double-denim-zip-up.JPEG"
+                ],
+                description: "Collection piece from Festival Szn.",
+                photographer: "TBC",
+                model: "TBC",
+                available: false,
+                storeLink: ""
+            },
+
+            {
+                name: "Hoodie",
+                images: [
+                    "assets/images/collections/festival-szn/hoodie.JPG",
+                    "assets/images/collections/festival-szn/hoodie-2.JPG",
+                    "assets/images/collections/festival-szn/hoodie-3.JPG",
+                    "assets/images/collections/festival-szn/hoodie-4.JPG"
+                ],
+                description: "Collection piece from Festival Szn.",
+                photographer: "TBC",
+                model: "TBC",
+                available: false,
+                storeLink: ""
+            },
+
+            {
+                name: "Zip-Up Y2K",
+                images: [
+                    "assets/images/collections/festival-szn/zip-up-y2k.JPEG",
+                    "assets/images/collections/festival-szn/zip-up-y2k-2.JPG",
+                    "assets/images/collections/festival-szn/zip-up-y2k-3.JPG",
+                    "assets/images/collections/festival-szn/zip-up-y2k-4.JPG"
+                ],
+                description: "Collection piece from Festival Szn.",
+                photographer: "TBC",
+                model: "TBC",
+                available: false,
+                storeLink: ""
+            }
+        ],
+
+        editorial: [
+            "assets/images/collections/festival-szn/cover-photo.JPG",
+
+            "assets/images/collections/festival-szn/skate-3-group.JPG",
+            "assets/images/collections/festival-szn/skate-3-group-2.JPG",
+            "assets/images/collections/festival-szn/skate-3-group-3.JPG",
+            "assets/images/collections/festival-szn/skate-3-group-4.JPG",
+
+            "assets/images/collections/festival-szn/skate-2-group.JPG",
+            "assets/images/collections/festival-szn/skate-2-group-2.JPG",
+            "assets/images/collections/festival-szn/skate-2-group-3.JPG",
+            "assets/images/collections/festival-szn/skate-2-group-4.JPG",
+
+            "assets/images/collections/festival-szn/skate-5-group.JPG",
+
+            "assets/images/collections/festival-szn/alleyway-2-group.JPEG"
+        ]
     }
 ];
