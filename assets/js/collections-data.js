d3acc1460c3954a54e03941d5f885cd9215a64f0
LiamGrimes39 <55996969+LiamGrimes39@users.noreply.github.com>
2026-08-23 15:54:51 +0100

    Add SS26 collection image data and editorial gallery

--- assets/js/collections-data.js
+++ assets/js/collections-data.js
@@ -163,5 +163,179 @@ window.SPERRIN_COLLECTIONS.COLLECTIONS = [
 
             "assets/images/collections/festival-szn/alleyway-2-group.JPEG"
         ]
+    },
+    
+    {
+        name: "SS26",
+        slug: "ss26",
+        description: "Sperrin Design Spring Summer 2026 collection.",
+        coverImage: "assets/images/collections/SS26/DSCF1032.JPEG",
+
+        pieces: [
+            {
+                name: "Outfit 1",
+                images: [
+                    "assets/images/collections/SS26/2442C6AE-76A5-4F70-94F4-CBFFA721C143.JPEG",
+                    "assets/images/collections/SS26/DSCF7963.JPEG",
+                    "assets/images/collections/SS26/3B12CACA-B241-4F40-9AEE-8CE1CBBAC3AA.JPEG",
+                    "assets/images/collections/SS26/5F1AC302-9EAD-4591-9039-F2B42E6E2A76.JPEG",
+                    "assets/images/collections/SS26/9CCD7632-EEB8-4B20-B7DF-CAA8025BF88A.JPEG"
+                ],
+                description: "",
+                photographer: "TBC",
+                model: "TBC",
+                available: false,
+                storeLink: ""
+            },
+
+            {
+                name: "Outfit 2",
+                images: [
+                    "assets/images/collections/SS26/3A29C085-A9DB-4E4A-8E1D-BFE371FCD19B.JPEG",
+                    "assets/images/collections/SS26/80CAA550-45B5-41D9-9BFE-DBB759583EFE.JPEG",
+                    "assets/images/collections/SS26/1650691E-BACB-48C4-8CF7-8534001ED483.JPEG",
+                    "assets/images/collections/SS26/E7E879DB-7E3A-46D5-B7C4-C4E1A5D2F329.JPEG"
+                ],
+                description: "",
+                photographer: "TBC",
+                model: "TBC",
+                available: false,
+                storeLink: ""
+            },
+
+            {
+                name: "Outfit 3",
+                images: [
+                    "assets/images/collections/SS26/2BD7211E-124B-4FD0-A52A-EB3E45C389C7.JPEG"
+                ],
+                description: "",
+                photographer: "TBC",
+                model: "TBC",
+                available: false,
+                storeLink: ""
+            },
+
+            {
+                name: "Outfit 4",
+                images: [
+                    "assets/images/collections/SS26/20B84631-76CC-4CC5-AF7C-24C7ABAB3C6F.JPEG",
+                    "assets/images/collections/SS26/CF4C54C5-7977-4C20-9E84-8691FD248A3C.JPEG"
+                ],
+                description: "",
+                photographer: "TBC",
+                model: "TBC",
+                available: false,
+                storeLink: ""
+            },
+
+            {
+                name: "Outfit 5",
+                images: [
+                    "assets/images/collections/SS26/CF4C54C5-7977-4C20-9E84-8691FD248A3C.JPEG"
+                ],
+                description: "",
+                photographer: "TBC",
+                model: "TBC",
+                available: false,
+                storeLink: ""
+            },
+
+            {
+                name: "Outfit 6",
+                images: [
+                    "assets/images/collections/SS26/3798D055-4CF2-4F02-B673-972B588BA451.JPEG",
+                    "assets/images/collections/SS26/C1ACE07B-B941-44A4-8D06-2870AA90B0C0.JPEG",
+                    "assets/images/collections/SS26/IMG_1942.JPG",
+                    "assets/images/collections/SS26/IMG_1949.JPG"
+                ],
+                description: "",
+                photographer: "TBC",
+                model: "TBC",
+                available: false,
+                storeLink: ""
+            },
+
+            {
+                name: "Outfit 7",
+                images: [
+                    "assets/images/collections/SS26/AB231ADE-4442-424F-9EE1-68AED45D9C09.JPEG"
+                ],
+                description: "",
+                photographer: "TBC",
+                model: "TBC",
+                available: false,
+                storeLink: ""
+            },
+
+            {
+                name: "Outfit 8",
+                images: [
+                    "assets/images/collections/SS26/BelfastFashionChloeJayne2-8.JPEG",
+                    "assets/images/collections/SS26/BelfastFashionChloeJayne2-9.JPEG",
+                    "assets/images/collections/SS26/BelfastFashionChloeJayne2-11.JPEG"
+                ],
+                description: "",
+                photographer: "TBC",
+                model: "TBC",
+                available: false,
+                storeLink: ""
+            },
+
+            {
+                name: "Outfit 9",
+                images: [
+                    "assets/images/collections/SS26/BF1BF866-15C3-49F9-AD80-A51F1B91AFD1.JPEG",
+                    "assets/images/collections/SS26/BFS_Brandy-02.JPG"
+                ],
+                description: "",
+                photographer: "TBC",
+                model: "TBC",
+                available: false,
+                storeLink: ""
+            },
+
+            {
+                name: "Outfit 10",
+                images: [
+                    "assets/images/collections/SS26/DSCF0267.JPEG",
+                    "assets/images/collections/SS26/DSCF0397.JPEG"
+                ],
+                description: "",
+                photographer: "TBC",
+                model: "TBC",
+                available: false,
+                storeLink: ""
+            },
+
+            {
+                name: "Outfit 11",
+                images: [
+                    "assets/images/collections/SS26/DSCF0684.JPEG",
+                    "assets/images/collections/SS26/IMG_1938.JPG"
+                ],
+                description: "",
+                photographer: "TBC",
+                model: "TBC",
+                available: false,
+                storeLink: ""
+            }
+        ],
+
+        editorial: [
+            "assets/images/collections/SS26/108C952B-37B7-4AEE-AB5E-3F5967AAE01E.JPEG",
+            "assets/images/collections/SS26/8485C5DC-E84C-4E8D-93D5-4033C504160B.JPEG",
+            "assets/images/collections/SS26/A1650BF9-15A9-42ED-96F5-0B13E357AA73.JPEG",
+            "assets/images/collections/SS26/BelfastFashionChloeJayne2-24.JPEG",
+            "assets/images/collections/SS26/C889E614-A842-4EF9-9B40-5E7A23B070CD.JPEG",
+            "assets/images/collections/SS26/DSCF0533.JPEG",
+            "assets/images/collections/SS26/DSCF0648.JPEG",
+            "assets/images/collections/SS26/DSCF1023.JPEG",
+            "assets/images/collections/SS26/DSCF1032.JPEG",
+            "assets/images/collections/SS26/9BCF4F5D-E559-4B59-B392-FCF79285636A.JPEG",
+            "assets/images/collections/SS26/29E3BD0B-B881-4F75-AB17-315B087A32A3.JPEG",
+            "assets/images/collections/SS26/35AF2D28-9018-41DA-AACD-1452CC502B27.JPEG",
+            "assets/images/collections/SS26/60F30F47-2825-4D9A-83EB-5BA2E652A7C0.JPEG"
+        ]
     }
+
 ];
